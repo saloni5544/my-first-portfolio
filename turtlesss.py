c9e2fa952da87e1d85112101525d21787d3fb527
saloni5544 <saloni.k.sahni@gmail.com>
2017-07-06 15:36:10 -0500

    Update turtlesss.py

--- turtlesss.py
+++ turtlesss.py
@@ -1,3 +1,14 @@
+from turtle import *
+import math
+
+
+sides = int(input(' How many sides do you want in your house base? '))
+
+# Name your Turtle.
+t = Turtle()
+
+
+# Set Up your screen and starting position.
 setup(0,0)
 x_pos = 0
 y_pos = 0
@@ -5,16 +16,14 @@ t.setposition(x_pos, y_pos)
 
 ### Write your code below:
 t.pendown()
-t.speed (3))
-t.pencolor("chocolate")
-t.forward(50)
-t.right(90)
-t.forward(50)
-t.right(90)
-t.forward(50)
-t.right(90)
-t.forward(50)
+t.speed (3)
+
+for side in range (0,sides):
+    t.forward(80)
+    t.right(360/sides)
 t.penup()
+
+
 
 t.pendown()
 t.pencolor("deepPink1")
