a7b3865b9f75d955bcbb9657557a5108138947f9
saloni5544 <saloni.k.sahni@gmail.com>
2017-07-06 14:12:47 -0500

    Update turtlesss.py

--- turtlesss.py
+++ turtlesss.py
@@ -1,1 +1,33 @@
+setup(0,0)
+x_pos = 0
+y_pos = 0
+t.setposition(x_pos, y_pos)
 
+### Write your code below:
+t.pendown()
+t.speed (3))
+t.pencolor("chocolate")
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.penup()
+
+t.pendown()
+t.pencolor("deepPink1")
+t.right(60)
+t.forward(50)
+t.right(60)
+t.forward(50)
+t.right(-60)
+t.forward(50)
+
+
+
+
+
+# Close window on click.
+exitonclick()
